499f5dd85d92cb57bce32a573f1da9d19ae76367
Bram Oosterhuis <dev@bybram.com>
2026-08-31 16:19:53 +0000

    Rename Core::Sink<T> to Core::SinkType<T> for Thunder 5.3
    Thunder 5.x renamed Core::Sink<T> to Core::SinkType<T>. The old name
    is kept as a using-alias (Services.h) so this is not a build break,
    but the alias is not guaranteed to survive future Thunder releases.
    Mechanical rename, no functional change.

--- plugin/Monitor.h
+++ plugin/Monitor.h
@@ -1070,7 +1070,7 @@ POP_WARNING()
     private:
         uint8_t _skipURL;
         Config _config;
-        Core::Sink<MonitorObjects> _monitor;
+        Core::SinkType<MonitorObjects> _monitor;
 
     private:
         void RegisterAll();
